969c36a8ebc4e28b9cb8f32399613274ee2abe18
StyleShit <32631382+StyleShit@users.noreply.github.com>
2024-12-21 20:33:59 +0200

    refactor: extract logic into `createStorageAdapter`

---
+++ .changeset/chatty-apricots-look.md
@@ -0,0 +1,5 @@
+---
+'storagik': patch
+---
+
+Refactor `createUseStorage`
--- src/create-use-storage.ts
+++ src/create-use-storage.ts
@@ -1,59 +1,10 @@
 import { useEffect, useReducer } from 'react';
+import { createStorageAdapter } from './create-storage-adapter';
 
 type Updater<T> = (prev: T) => T;
 
 export function createUseStorage(storageArea: Storage) {
-	// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
-	const getItem = <T>(key: string): T | null => {
-		const value = storageArea.getItem(key);
-
-		if (value === null) {
-			return null;
-		}
-
-		try {
-			return JSON.parse(value) as T;
-		} catch {
-			return null;
-		}
-	};
-
-	const setItem = (key: string, value: unknown) => {
-		const stringifiedValue = JSON.stringify(value);
-
-		storageArea.setItem(key, stringifiedValue);
-
-		// The browser doesn't dispatch the `storage` event for the current tab,
-		// so we need to dispatch it manually.
-		//
-		// https://developer.mozilla.org/en-US/docs/Web/API/Window/storage_event
-		window.dispatchEvent(
-			new StorageEvent('storage', {
-				key,
-				storageArea,
-			}),
-		);
-	};
-
-	const subscribe = (key: string, subscriber: () => void) => {
-		const abortController = new AbortController();
-
-		window.addEventListener(
-			'storage',
-			(e) => {
-				if (e.key !== key || e.storageArea !== storageArea) {
-					return;
-				}
-
-				subscriber();
-			},
-			{ signal: abortController.signal },
-		);
-
-		return () => {
-			abortController.abort();
-		};
-	};
+	const { getItem, setItem, subscribe } = createStorageAdapter(storageArea);
 
 	return <T>(key: string, initialState: T) => {
 		const [, reRender] = useReducer((p) => !p, false);
